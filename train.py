9133b0cddfccb9b51aa6484b40c6609ab6f6c82f
kdsen <408663188@qq.com>
2026-03-18 23:13:33 +0800

    auto pade m4 n1 k5 i4

--- train.py
+++ train.py
@@ -71,9 +71,9 @@ METRIC_TOLERANCE = 1e-6
 @dataclass
 class ExperimentConfig:
     # Edit this block between experiments.
-    description: str = "auto candidate [2/3] pade 100img"
-    pade_m: int = 2
-    pade_n: int = 3
+    description: str = "auto candidate [4/1] pade 100img"
+    pade_m: int = 4
+    pade_n: int = 1
     max_order: int = 5
     interval: int = 4
     enable_pade: bool = True
